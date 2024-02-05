c3100d5832eeb9bbc33c35c96ffbba12b978ec68
devenderswami <devenderswami53@gmail.com>
2024-02-05 22:43:48 +0530

    Time: 42 ms (98.19%), Space: 49 MB (25.19%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length ===0){
+        return ""
+    }
+
+    let prefix = strs[0]
+
+    for(let word of strs){
+        while(word.indexOf(prefix) !== 0){
+            prefix = prefix.slice(0,-1)
+            if(prefix === '') return ''
+        }
+    }
+    return prefix
+};
